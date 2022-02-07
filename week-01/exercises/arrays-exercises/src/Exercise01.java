8e0c6ea2574f1cccd0897ba594edbf29b37beb90
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-02-07 16:08:34 -0600

    Finishing exercise01

--- week-01/exercises/arrays-exercises/src/Exercise01.java
+++ week-01/exercises/arrays-exercises/src/Exercise01.java
@@ -4,5 +4,12 @@ public class Exercise01 {
         String[] legendaryCreatures = {"fairies", "goblins", "gnomes", "unicorns", "harpies", "solar fish"};
         System.out.println(legendaryCreatures[2]); // third creature - gnomes
         // 1. Print the first, fourth, and sixth creature to the console.
+        int[] chosen = {0,3,5};
+             for (int item : chosen) {
+                 System.out.println(legendaryCreatures[item]);
+             }
+        {
+
+        }
     }
 }
