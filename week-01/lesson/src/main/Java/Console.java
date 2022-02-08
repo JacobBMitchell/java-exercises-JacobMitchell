e7555643e2c028a9778588ce41487550750451ef
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-02-08 16:29:21 -0600

    Following 2-8-22 reading

---
+++ week-01/lesson/src/main/Java/Console.java
@@ -0,0 +1,13 @@
+package main.Java;
+
+import java.util.Scanner;
+
+public class Console {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scnr.nextLine();
+        System.out.println("Yo what is up " + name);
+        System.out.printf("Hey its you %s what is up dog, how is your %s?", name, "mom");
+    }
+}
