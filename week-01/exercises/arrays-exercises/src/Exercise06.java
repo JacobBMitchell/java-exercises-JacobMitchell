61b747951771a3bd94f9df4653a0cc4635afdaab
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-02-07 20:46:29 -0600

    Finishing exercise04 & exercise05

--- week-01/exercises/arrays-exercises/src/Exercise06.java
+++ week-01/exercises/arrays-exercises/src/Exercise06.java
@@ -4,6 +4,12 @@ public class Exercise06 {
         int[] values = {18, 42, 54, 93, 22};
 
         // 1. Create a loop to calculate the sum of elements in `values`.
+        int sum = 0;
+        for (int value:values
+             ) {
+            sum += value;
+        }
         // 2. Print the result.
+        System.out.println(sum);
     }
 }
