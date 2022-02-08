b91d6c62733878bcc1e58b808f3193c43ae193c1
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-02-08 11:16:01 -0600

    Hello World

--- week-01/lesson/src/Lesson1.java
+++ week-01/lesson/src/Lesson1.java
@@ -1,5 +1,6 @@
 public class Lesson1 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String hello = "Hello World";
+        System.out.println(hello);
     }
 }
