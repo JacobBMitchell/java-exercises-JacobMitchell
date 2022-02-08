a0f4792b20b510a052459f296c578e58b2ffa9f4
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-02-07 20:39:55 -0600

    Finishing exercise04 & exercise05

--- week-01/exercises/arrays-exercises/src/Exercise04.java
+++ week-01/exercises/arrays-exercises/src/Exercise04.java
@@ -2,7 +2,15 @@ public class Exercise04 {
 
     public static void main(String[] args) {
         // 1. Declare an array to hold the names of the world's oceans.
+        String[] oceans = {"Atlantic", "Pacific", "Indian", "Arctic"};
         // Set its value using array literal notation.
+       /* oceans[0] = "Atlantic";
+        oceans[1] = "Pacific";
+        oceans[2] = "Indian";
+        oceans[3] = "Arctic";*/
         // 2. Loop over each element and print it.
+        for (String ocean:oceans){
+            System.out.println(ocean);
+        }
     }
 }
--- week-01/exercises/arrays-exercises/src/Exercise05.java
+++ week-01/exercises/arrays-exercises/src/Exercise05.java
@@ -2,7 +2,19 @@ public class Exercise05 {
 
     public static void main(String[] args) {
         // 1. Declare an array to hold the names of the world's continents.
+        String[] continents = new String[7];
         // Do not use array literal notation. Allocate space for 6 continents and then set each value by index.
+        continents[0] = "North America";
+        continents[1] = "South America";
+        continents[2] = "Europe";
+        continents[3] = "Africa";
+        continents[4] = "Asia";
+        continents[5] = "Australia";
+        continents[6] = "Antarctica";
         // 2. Loop over each element and print it.
+        for (int i = 0; i < continents.length; i++) {
+            System.out.println(continents[i]);
+
+        }
     }
 }
