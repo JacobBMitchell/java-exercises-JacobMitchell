2a1e9d16b5a474afb8f456e8a1087524f8929886
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-02-11 09:45:01 -0600

    Learning the ctrl-shift-r rename trick

--- week-01/exercises/arrays-exercises/src/Exercise01.java
+++ week-01/exercises/arrays-exercises/src/Exercise01.java
@@ -4,12 +4,9 @@ public class Exercise01 {
         String[] legendaryCreatures = {"fairies", "goblins", "gnomes", "unicorns", "harpies", "solar fish"};
         System.out.println(legendaryCreatures[2]); // third creature - gnomes
         // 1. Print the first, fourth, and sixth creature to the console.
-        int[] chosen = {0,3,5};
-             for (int item : chosen) {
-                 System.out.println(legendaryCreatures[item]);
+        int[] selected = {0,3,5};
+             for (int index : selected) { //ctrl-shift-r to swap all of one variable
+                 System.out.println(legendaryCreatures[index]);
              }
-        {
-
-        }
     }
 }
