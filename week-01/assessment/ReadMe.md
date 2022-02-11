b75ab2b1c8b3c4990b3e53dff38afb6e26705e0a
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-02-11 15:29:29 -0600

    Creating rough draft of README.md file

---
+++ week-01/assessment/ReadMe.md
@@ -0,0 +1,31 @@
+
+## High Level Description
+Create a command line hotel that asks for capacity, check in guests, checkout guests, and show guests +- 5 from a given room.
+I need to use variables, arrays, methods, if statements, and for/while loops, and use Scanners to get user input.
+
+## Functional Requirements
+Here you should list out requirements for my project from the provided assessment documentation:
+* [ ] Initial inquiry about how many rooms.
+* [ ] Create menu: Check in, out, view guests, exit
+* [ ] Create methods for each menu option
+
+###Check-in
+Ask for room using scanner
+Check to see if room is available
+Ask for name and pair the two in the main array
+confirm check in
+
+###Check-out
+Ask for room using scanner
+check if room is occupied
+remove the occupant of the room
+confirm checkout
+
+###View guests
+Scanner ask for room#
+Check +5 from input if greater than rooms available start over from room 1(0)
+Check -5 from input, arrays should allow negative numbers to check backwards
+display rooms
+
+###Exit
+boolean an exit condition to be false and cancel while loop
--- week-01/assessment/src/main/java/CapsuleHotel.java
+++ week-01/assessment/src/main/java/CapsuleHotel.java
@@ -3,6 +3,58 @@ import java.util.Scanner;
 public class CapsuleHotel {
     public static Scanner scn = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        int numberOfRooms;
+        while(true) {
+            System.out.print("Hello and Welcome to Capsule Hotel! How many rooms do you have? ");
+            numberOfRooms = scn.nextInt();
+            if(numberOfRooms >= 0){
+                System.out.printf("You have created a hotel with %d rooms! \n",numberOfRooms);
+                break;
+            }
+        }
+        String[] rooms = new String[numberOfRooms];
+        lab1: while(true){
+            printMenuOptions();
+            System.out.print("Choose an option [1-4]: ");
+            int choice = scn.nextInt();
+            switch (choice){
+                case 1:
+                    checkIn(rooms);
+                    break;
+                case 2:
+                    checkOut(rooms);
+                    break;
+                case 3:
+                    viewRooms(rooms);
+                    break;
+                case 4:
+                    break lab1;
+                default:
+                    continue;
+            }
+        }
+    }
+
+    private static void viewRooms(String[] rooms) {
+
+    }
+
+    private static void checkOut(String[] rooms) {
+
+    }
+
+    private static void checkIn(String[] rooms) {
+        System.out.println("What room would you like to check into? ");
+        int newRoom = scn.nextInt();
+
+    }
+
+    private static void printMenuOptions() {
+        System.out.println("Guest Menu \n ==========");
+        System.out.println("1. Check In");
+        System.out.println("2. Check Out");
+        System.out.println("3. View Guests");
+        System.out.println("4. Exit");
+
     }
 }
