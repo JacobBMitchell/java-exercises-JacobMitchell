fe3b50067fdf9f2e4a82bb1a935921e05bb678ea
JacobBMitchell <mattdamionoriginal@gmail.com>
2022-02-22 20:45:59 -0600

    working along, memories, venus

---
+++ week-03/memories/src/test/java/memories/data/MemoryFileRepositoryTest.java
@@ -0,0 +1,30 @@
+package memories.data;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemoryFileRepositoryTest {
+
+    @org.junit.jupiter.api.Test
+    void findAll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void findShareable() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteById() {
+    }
+}
