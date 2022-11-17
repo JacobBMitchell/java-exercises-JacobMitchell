7b809ccca4239fe937394d16a1322336e45bc871
Jacob Mitchell <jacobmitchell@dev-10.com>
2022-11-17 00:16:20 -0600

    bit o practice

---
+++ warmups/src/main/java/warmups/PrimeAverage.java
@@ -0,0 +1,39 @@
+package warmups;
+
+public class PrimeAverage {
+    static int prev = 1;
+    public static void main(String[] args) {
+        //loop over primes
+        //2+3/2 ) + 5)/3 +7)4 ...
+        double primeAvg =0;
+        for (int i = 1; i < 100; i++) {
+            prev = findNextPrime(prev);
+            primeAvg = (prev+primeAvg)/i;
+            System.out.println(i+": \n    Prime Average: "+ primeAvg +"\n    Prime: " +prev);
+        }
+    }
+
+    private static int findNextPrime(int prev) {
+        boolean primeFound = false;
+        prev = prev + 1;
+        if (prev < 4){
+            System.out.println("prev: " + prev);
+            return prev ;
+        }
+        for (int i = prev; !primeFound; i++) {
+            double jMax = Math.sqrt(i);
+            for (int j = 2; j <= jMax; j++) {
+                if (i%j == 0){
+                    break;
+                }
+                if (j + 1 > jMax){
+                    primeFound = true;
+                }
+            }
+            if(primeFound){
+                prev = i;
+            }
+        }
+        return prev;
+    }
+}
