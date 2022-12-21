97dcb123cf6e44eed7de87b450dd033bb683baca
Jacob Mitchell <jacobmitchell@dev-10.com>
2022-12-20 22:53:39 -0600

    Practices

---
+++ warmups/src/main/java/warmups/LongestPalSubString.java
@@ -0,0 +1,57 @@
+package warmups;
+
+public class LongestPalSubString {
+    public static void main(String[] args) {
+        String input = "fortbragstoplistpopracecardigdug"; //Icarat
+        String output = LPSS(input);
+        System.out.println(output);
+    }
+
+
+/*
+    LPSS Longest Palindromic sub string
+    This function takes a string
+    returns a string that is the largest palindrom substring
+ */
+    public static String LPSS(String input) {
+        //edge case for empty strings
+        if (input.length() == 0){
+            return "";
+        }
+        String output = "";
+        //loops over all letters in the string from left and narrowing to the right
+        for (int i = 0; i < input.length(); i++) {
+            for (int j = input.length()-1; j > i; j--) {
+                //checks for palindrome potential
+                if (input.charAt(i) == input.charAt(j)){
+                    String sub = input.substring(i,j+1);
+                    //checks if the sub string is a palidrome
+                    if(palindromeCheck(sub)) {
+                        //retains largest palindrome
+                        if (sub.length() > output.length()) {
+                            output = sub;
+                        }
+                    }
+                }
+
+            }
+        }
+        if (output.equals("")){
+            return input.charAt(0) + "";
+        }
+        return output;
+    }
+
+    //checks to see if a string is a palindrome
+    //takes a string
+    //returns a boolean if the string is a palindrome
+    private static Boolean palindromeCheck(String sub) {
+        for (int i = 0; i < sub.length()/2; i++) {
+            if (sub.charAt(i) != sub.charAt(sub.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
--- warmups/src/main/java/warmups/PrimeAverage.java
+++ warmups/src/main/java/warmups/PrimeAverage.java
@@ -6,10 +6,12 @@ public class PrimeAverage {
         //loop over primes
         //2+3/2 ) + 5)/3 +7)4 ...
         double primeAvg =0;
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < 500; i++) {
             prev = findNextPrime(prev);
+            double temp = primeAvg;
             primeAvg = (prev+primeAvg)/i;
-            System.out.println(i+": \n    Prime Average: "+ primeAvg +"\n    Prime: " +prev);
+            System.out.println(i+": \n    Prime Average: "+ primeAvg +"\n    Prime: " +prev
+                    +"\n    Ratio: " +primeAvg/temp);
         }
     }
 
